56b29106168dbad5740da1c467270143c4677ca0
Wiebe <wiebesteverink@hotmail.com>
2015-01-20 23:37:07 -0800

    Added aanvraag scherm

--- MarioEnCo/app/src/main/java/ws/marioenco/AanvraagActivity.java
+++ MarioEnCo/app/src/main/java/ws/marioenco/AanvraagActivity.java
@@ -6,17 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import ws.marioenco.Helpers.ClientHelper;
@@ -26,7 +20,7 @@ import ws.marioenco.Models.ServiceLijstModel;
 import ws.marioenco.Models.Settings;
 
 
-public class ServiceActivity extends Activity  {
+public class AanvraagActivity extends Activity  {
 
     // public String ipAdress = "192.168.56.1";
     TextView serviceTag,serviceInfo;
@@ -34,26 +28,27 @@ public class ServiceActivity extends Activity  {
 
     Settings settingsData = Settings.getInstance();
     ServiceLijstModel serviceLijstModel = ServiceLijstModel.getInstance();
-
     InformatieServiceModel informatieServiceModel = InformatieServiceModel.getInstance();
+    InformatieServiceBeknoptModel informatieServiceBeknoptModel = InformatieServiceBeknoptModel.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service);
+        setContentView(R.layout.activity_aanvraag);
 
-          serviceTag = (TextView) findViewById(R.id.serviceTextViewService);
-          serviceInfo = (TextView) findViewById(R.id.servicesInfoLong);
+          serviceTag = (TextView) findViewById(R.id.serviceTextViewServiceAanvraag);
+          serviceInfo = (TextView) findViewById(R.id.servicesInfoAanvraag);
 
 //// TODO data gesaved ophalen
 //            list.add("Riolering1");
 //            list.add("Lekkage1");
 //            list.add("Prinses in nood1");
 
-        getServicesInfoLong();
-
+        // TONEN VAN GESELECTEERDE SERVICE
         serviceTag.setText(serviceLijstModel.getServicesLijst().get(serviceLijstModel.getSelectedService()));
+        // Tonen van beknopte info
+        serviceInfo.setText(informatieServiceBeknoptModel.getShortInfoService());
         }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,56 +72,16 @@ public class ServiceActivity extends Activity  {
         return super.onOptionsItemSelected(item);
     }
 
-// Functie om de  informatie op te halen en het model te vullen
-    public void getServicesInfoLong() {
-        //aanmaken van een nieuw jsonobject
-        JSONObject infoObject = new JSONObject();
 
-        try {
-            //verzenden van het jsonobject
-            infoObject.put("informatie",(serviceLijstModel.getServicesLijst().get(serviceLijstModel.getSelectedService())));
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        String reactie = "string";
-        {
-            //servercommunicator proberen te verbinden met de server
-            try {
-                reactie = new ClientHelper(this, settingsData.getIp4Adress(), 4444, infoObject.toString()).execute().get();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                JSONObject info = new JSONObject(reactie);
-
-                String infoString = info.getString("informatie");
-
-                informatieServiceModel.setinfoService(infoString);
-
-                serviceInfo.setText(informatieServiceModel.getinfoService());
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
 // functie om met de button verder te gaan naar de volgende pagina.
     public void nextPage(View view){
 
-        Intent intent = new Intent(this, AanvraagActivity.class);
-        startActivity(intent);
-        finish();
     }
 
     // functie om met de button verder te gaan naar de vorige pagina.
     public void prevPage(View view){
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, ServiceActivity.class);
         startActivity(intent);
         finish();
     }
